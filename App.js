55bcd6a8251bc71c6145899fa01391f315170fc6
Tidbits JS <tidbitsjs@gmail.com>
2022-03-14 23:54:51 +0530

    app setup

---
+++ App.js
@@ -0,0 +1,42 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  useColorScheme,
+} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import Input from './src/components/Input';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={isDarkMode ? Colors.darker : Colors.lighter}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Input />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default App;
---
+++ src/components/Input.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text, TextInput} from 'react-native';
+
+const Input = () => {
+  return (
+    <View>
+      <Text>Input</Text>
+      <TextInput placeholder="some input" />
+    </View>
+  );
+};
+
+export default Input;
